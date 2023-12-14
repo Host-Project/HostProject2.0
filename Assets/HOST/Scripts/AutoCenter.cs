6408a57579e11e96c59fd977c81fbcaf63eb42e7
Quentin Forestier <quentin.forestier@heig-vd.ch>
2023-12-14 15:14:55 +0100

    Working on anchor, should be placed at center and accessible in others scenes

--- Assets/HOST/Scripts/AutoCenter.cs
+++ Assets/HOST/Scripts/AutoCenter.cs
@@ -1,39 +1,26 @@
-using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-using UnityEngine.SceneManagement;
 
-public class SceneContainer : MonoBehaviour
+public class AutoCenter : MonoBehaviour
 {
     private bool isPlaced = false;
-    [SerializeField]
-    private bool hideSceneAnchor = false;
     // Start is called before the first frame update
     void Start()
     {
+        
     }
-
-   
-
 
     // Update is called once per frame
     void Update()
     {
-        AutoCenter anchor = FindAnyObjectByType<AutoCenter>();
+        SceneAnchor anchor = FindAnyObjectByType<SceneAnchor>();
         if (anchor != null && !isPlaced)
         {
-            Debug.Log("Placing scene");
-            Debug.Log(gameObject.name);
             transform.position = anchor.transform.position;
             transform.rotation = Quaternion.Euler(transform.rotation.x, anchor.transform.GetChild(0).rotation.y, transform.rotation.z);
             //new Quaternion.EulerAngles(anchor.transform.rotation.x, anchor.transform.rotation.y, anchor.transform.rotation.z);
             isPlaced = true;
-
-            if (hideSceneAnchor)
-            {
-                anchor.transform.gameObject.SetActive(false);
-            }
         }
     }
 }
